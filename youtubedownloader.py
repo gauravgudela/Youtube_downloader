12930afac23fa9ebe3c6439f08280d44dca31276
gaurav <gauravgudela@gmail.com>
2025-08-27 15:58:21 +0530

    he he he ha

---
+++ youtubedownloader.py
@@ -0,0 +1,34 @@
+import yt_dlp
+import os
+def download_youtube_video(url,output_path="Downloads"):
+    try:
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+        
+        ydl_opts ={
+            'format':'best',
+            'outtmpl':f'{output_path}/%(title)s.%(ext)s',
+            'noplaylist': True,
+        }
+        print(f"downloading:{url}")
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        print(f"Download completed and the video saved to {output_path}")
+
+    
+
+    except yt_dlp.utils.DownloadError as de:
+        print(f"Download error: {str(de)}")
+        print("\nhere are the available formats..")
+        try:
+            with yt_dlp.YoutubeDL({'listformats': True}) as ydl:
+                ydl.download([url])
+        except Exception as e:
+            print(f"Failed to list formats: {str(e)}")
+    except Exception as e:
+        print(f"An error occured: {str(e)}")
+
+
+if __name__ == "__main__":
+    video_url =input("Enter thy youtube url: ")
+    download_youtube_video(video_url)
